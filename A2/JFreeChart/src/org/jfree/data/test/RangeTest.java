5a3826f7f87926ad9558e02413f9dc48f9124e58
Sjnaka <wu.dingkai@hotmail.com>
2024-02-10 18:06:11 -0700

    updated report and RangeTest

--- A2/JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ A2/JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -5,134 +5,162 @@ import org.jfree.data.Range;
 import org.junit.*;
 
 public class RangeTest {
-    private Range testRange;
-    
-    @BeforeClass public static void setUpBeforeClass() throws Exception {
-    }
-
+	private Range positiveRange;
+	private Range negativeRange;
+	private Range zeroRange;
+	private Range minMaxRange;
+	
     @Before
     public void setUp() throws Exception {
-    	testRange = new Range(-5, 5);
-    }
-
-    @Test
-    public void getCentralValueZeroTest() {
-        assertEquals("The central value of -5 and 5 should be 0",
-        		0, testRange.getCentralValue(), .000000001d);
+    	positiveRange = new Range(4, 6);
+    	negativeRange = new Range(-8, -4);
+    	zeroRange = new Range(0, 0);
+    	minMaxRange = new Range(Double.MIN_VALUE, Double.MAX_VALUE);
     }
     
     @Test
     public void getCentralValuePositiveTest() {
-    	Range positiveRange = new Range(4, 6);
     	assertEquals("The central value of 4 and 6 should be 5",
     	        5, positiveRange.getCentralValue(), .000000001d);
     }
     
     @Test
     public void getCentralValueNegativeTest() {
-    	Range negativeRange = new Range(-8, -4);
         assertEquals("The central value of -8 and -4 should be -6",
         		-6, negativeRange.getCentralValue(), .000000001d);
     }
     
     @Test
+    public void getCentralValueZeroTest() {
+        assertEquals("The central value of 0 and 0 should be 0",
+        		0, zeroRange.getCentralValue(), .000000001d);
+    }
+    
+    @Test
     public void getCentralValueMinMaxTest() {
-    	Range range = new Range(Double.MIN_VALUE, Double.MAX_VALUE);
     	double medium = (Double.MAX_VALUE + Double.MIN_VALUE)/2;
         assertEquals("The central value of MIN_VALUE and MIN_VALUE should be " + medium,
-        		medium, range.getCentralValue(), .000000001d);
+        		medium, minMaxRange.getCentralValue(), .000000001d);
     }
     
     @Test
     public void getUpperBoundPositiveTest() {
-    	assertEquals("The upperbound of -5 and 5 should be 5",
-    	        5, testRange.getUpperBound(), .000000001d);
+    	assertEquals("The upperbound of 4 and 6 should be 6",
+    	        5, positiveRange.getUpperBound(), .000000001d);
     }
     
     @Test
     public void getUpperBoundNegativeTest() {
-    	Range negativeRange = new Range(-10, -9);
-    	assertEquals("The upperbound of -10 and -9 is -9", -9,
+    	assertEquals("The upperbound of -8 and -4 should be -4", -4,
     			negativeRange.getUpperBound(), .000000001d);
     }
     
     @Test
-    public void getUpperBoundMinMaxTest() {
-    	Range range = new Range(Double.MIN_VALUE, Double.MAX_VALUE);
-    	assertEquals("The upperbound of MIN_VALUE and MIN_VALUE should be " + Double.MAX_VALUE, 
-    			Double.MAX_VALUE, range.getUpperBound(), .000000001d);
+    public void getUpperBoundZeroTest() {
+    	assertEquals("The upperbound of 0 and 0 should be 0", 0,
+    			zeroRange.getUpperBound(), .000000001d);
     }
     
     @Test
-    public void getLowerBoundNegativeTest() {
-    	assertEquals("The upperbound of -5 and 5 should be 5",
-    	        -5, testRange.getLowerBound(), .000000001d);
+    public void getUpperBoundMinMaxTest() {
+    	assertEquals("The upperbound of MIN_VALUE and MIN_VALUE should be " + Double.MAX_VALUE, 
+    			Double.MAX_VALUE, minMaxRange.getUpperBound(), .000000001d);
     }
     
     @Test
     public void getLowerBoundPositiveTest() {
-    	Range positiveRange = new Range(19, 25);
-    	assertEquals("The lower bound of 19 and 25 is 19", 19, positiveRange.getLowerBound(), .000000001d);
+    	assertEquals("The lower bound of 4 and 6 should be 4", 4, positiveRange.getLowerBound(), .000000001d);
     }
     
+    @Test
+    public void getLowerBoundNegativeTest() {
+    	assertEquals("The upperbound of -8 and -4 should be -8",
+    	        -8, negativeRange.getLowerBound(), .000000001d);
+    }
+    
+    @Test
+    public void getLowerBoundZeroTest() {
+    	assertEquals("The upperbound of 0 and 0 should be 0",
+    	        0, zeroRange.getLowerBound(), .000000001d);
+    }
+
     @Test
     public void getLowerBoundMinMaxTest() {
-    	Range range = new Range(Double.MIN_VALUE, Double.MAX_VALUE);
     	assertEquals("The lowerbound of MIN_VALUE and MIN_VALUE should be " + Double.MIN_VALUE, 
-    			Double.MIN_VALUE, range.getLowerBound(), .000000001d);
+    			Double.MIN_VALUE, minMaxRange.getLowerBound(), .000000001d);
     }
     
     @Test
-    //5 is also the upper_bound value in this range
     public void containsPositiveTest() {
-    	assertTrue("Value 5 is within -5 and 5",  testRange.contains(5));
+    	assertTrue("Value 5 is within 4 and 6",  positiveRange.contains(5));
     }
     
     @Test
     public void containsNegativeTest() {
-    	assertFalse("Value -6 is not within -5 and 5",  testRange.contains(-6));
+    	assertTrue("Value -6 is within -8 and -4",  negativeRange.contains(-6));
     }
     
     @Test
     public void containsZeroTest() {
-    	assertTrue("Value 0 is within -5 and 5",  testRange.contains(0));
+    	assertTrue("Value 0 is within 0 and 0",  zeroRange.contains(0));
     }
     
     @Test
     public void containsMinMaxTest() {
-    	Range range = new Range(Double.MIN_VALUE, Double.MAX_VALUE);
-    	assertTrue("Value " + Double.MIN_VALUE + " is within " + Double.MIN_VALUE + " and " + Double.MAX_VALUE,  range.contains(Double.MIN_VALUE));
-    	assertTrue("Value " + Double.MAX_VALUE + " is within " + Double.MIN_VALUE + " and " + Double.MAX_VALUE,  range.contains(Double.MAX_VALUE));
+    	assertTrue("Value " + Double.MIN_VALUE + " is within " + Double.MIN_VALUE + " and " + Double.MAX_VALUE,  minMaxRange.contains(Double.MIN_VALUE));
+    	assertTrue("Value " + Double.MAX_VALUE + " is within " + Double.MIN_VALUE + " and " + Double.MAX_VALUE,  minMaxRange.contains(Double.MAX_VALUE));
+    }
+    
+    @Test
+    public void containsAUBTest() {
+    	assertFalse("Value 7 is not within 4 and 6",  positiveRange.contains(7));
+    }
+    
+    @Test
+    public void containsBLBTest() {
+    	assertFalse("Value -9 is not within -8 and -4",  negativeRange.contains(-9));
     }
     
     @Test
     public void constrainPositiveTest() {
-    	assertEquals("The closed value from -5 to 5 to 6 is 5", 5, testRange.constrain(6), .000000001d);
+    	assertEquals("The closed value from (4, 6) to 6 is 6", 6, positiveRange.constrain(6), .000000001d);
     }
     
     @Test
     public void constrainNegativeTest() {
-    	assertEquals("The closed value from -5 to 5 to -6 is -5", -5, testRange.constrain(-6), .000000001d);
+    	assertEquals("The closed value from (-8, -4) to -8 is -8", -8, negativeRange.constrain(-8), .000000001d);
     }
     
     @Test
     public void constrainZeroTest() {
-    	assertEquals("The closed value from -5 to 5 to 0 is 0", 0, testRange.constrain(0), .000000001d);
+    	assertEquals("The closed value from (0, 0) to 0 is 0", 0, zeroRange.constrain(0), .000000001d);
     }
     
     @Test
     public void constrainMinMaxTest() {
-    	Range range = new Range(Double.MIN_VALUE, Double.MAX_VALUE);
-    	assertEquals("The closed value from " + Double.MIN_VALUE + " to " +  Double.MAX_VALUE + " to " + Double.MAX_VALUE + " is " + Double.MAX_VALUE,
-    			Double.MAX_VALUE, range.constrain(Double.MAX_VALUE), .000000001d);
-    	assertEquals("The closed value from " + Double.MIN_VALUE + " to " +  Double.MAX_VALUE + " to " + Double.MIN_VALUE + " is " + Double.MIN_VALUE,
-    			Double.MIN_VALUE, range.constrain(Double.MIN_VALUE), .000000001d);
+    	assertEquals("The closed value from (" + Double.MIN_VALUE + ", " +  Double.MAX_VALUE + ") to " + Double.MAX_VALUE + " is " + Double.MAX_VALUE,
+    			Double.MAX_VALUE, minMaxRange.constrain(Double.MAX_VALUE), .000000001d);
+    	assertEquals("The closed value from (" + Double.MIN_VALUE + ", " +  Double.MAX_VALUE + ") to " + Double.MIN_VALUE + " is " + Double.MIN_VALUE,
+    			Double.MIN_VALUE, minMaxRange.constrain(Double.MIN_VALUE), .000000001d);
     }
     
-    @After
-    public void tearDown() throws Exception {
-    	testRange = null;
+    @Test
+    public void constrainAUBTest() {
+    	assertEquals("The closed value from (4, 6) to 7 is 6", 6, positiveRange.constrain(6), .000000001d);
     }
+    
+    @Test
+    public void constrainBLBTest() {
+    	assertEquals("The closed value from (-8, -4) to -9 is -8", -8, negativeRange.constrain(-8), .000000001d);
+    }
+    
+	@After
+	public void tearDown() throws Exception {
+    	positiveRange = null;
+    	negativeRange = null;
+    	zeroRange = null;
+    	minMaxRange = null;
+	}
 
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
